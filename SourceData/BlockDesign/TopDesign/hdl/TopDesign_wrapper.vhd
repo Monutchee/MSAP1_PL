55a5271db95e9fe1279004c80a83ec4a802dcbc6
Lester Lo <21245380+lesterlo@users.noreply.github.com>
2026-08-18 14:09:34 -0400

    Fix the new component vhdl type

--- SourceData/BlockDesign/TopDesign/hdl/TopDesign_wrapper.vhd
+++ SourceData/BlockDesign/TopDesign/hdl/TopDesign_wrapper.vhd
@@ -2,7 +2,7 @@
 --Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2025.2 (lin64) Build 6299465 Fri Nov 14 12:34:56 MST 2025
---Date        : Tue Aug 18 13:52:44 2026
+--Date        : Tue Aug 18 14:08:12 2026
 --Host        : mnc1 running 64-bit Ubuntu 24.04.4 LTS
 --Command     : generate_target TopDesign_wrapper.bd
 --Design      : TopDesign_wrapper
